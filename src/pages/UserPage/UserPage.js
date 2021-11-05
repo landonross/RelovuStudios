0a2308359d909726304b6b3296b26a80d8f5d874
Landon Ross <landon.w.ross@gmail.com>
2021-11-05 11:47:36 -0600

    added profile page

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -11,6 +11,8 @@ import {
   //   UsersIcon,
   XIcon,
 } from '@heroicons/react/outline'
+import Footer from '../../components/Footer/Footer';
+import Profile from '../Profile/Profile';
 
 const navigation = [
   { name: 'Dashboard', href: '/', icon: HomeIcon, current: true },
@@ -113,7 +115,7 @@ export default function UserPage() {
                   </nav>
                 </div>
                 <div className="flex-shrink-0 flex bg-gray-700 p-4">
-                  <a href="#" className="flex-shrink-0 group block">
+                  <a href="." className="flex-shrink-0 group block">
                     <div className="flex items-center">
                       {/* <div>
                         <img
@@ -170,7 +172,7 @@ export default function UserPage() {
               </nav>
             </div>
             <div className="flex-shrink-0 flex bg-gray-700 p-4">
-              <a href="#" className="flex-shrink-0 w-full group block">
+              <a href="." className="flex-shrink-0 w-full group block">
                 <div className="flex items-center">
                   {/* <div>
                     <img
@@ -202,16 +204,13 @@ export default function UserPage() {
           <main className="flex-1">
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                {/* <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1> */}
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                {/* Replace with your content */}
-                <div className="py-4">
-                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-                </div>
-                {/* /End replace */}
+                  <Profile />
               </div>
             </div>
+            <Footer />
           </main>
         </div>
       </div>
